043ebc21120e6b99b0d00286af0ce4671270337b
khushi vish <vishkhushi.10@gmail.com>
2025-02-23 01:03:31 +0530

    added some changes

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ main()
   .catch((err) => console.log(err));
 
 async function main() {
-  await mongoose.connect("mongodb://127.0.0.1:27017/itchio");
+  await mongoose.connect(process.env.MONGO_URI);
 }
 
 // ROUTES
